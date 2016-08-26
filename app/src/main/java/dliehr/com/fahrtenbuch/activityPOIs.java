3c8ae68f059e7d758d67d9fbce21c0367aae6f3c
Dominik Liehr <dominik_liehr@me.com>
2016-08-27 01:59:49 +0200

    new permission model

---
+++ app/src/main/java/dliehr/com/fahrtenbuch/activityPOIs.java
@@ -0,0 +1,13 @@
+package dliehr.com.fahrtenbuch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activityPOIs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activity_pois);
+    }
+}
